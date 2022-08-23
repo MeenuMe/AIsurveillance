696e21c493107cb888028aeb9689085bd44f7b8a
MeenuMe <73883057+MeenuMe@users.noreply.github.com>
2022-08-23 17:45:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+objects = [];
+object_status = "";
 video = "";
+
 function preload(){
     video = createVideo('video.mp4');
     video.hide();
@@ -9,9 +12,7 @@ function setup() {
     canvas.center();
 }
 
-function draw() {
-    image(video, 0, 0, 480, 380);
-}
+
 
 function start() {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
@@ -24,4 +25,30 @@ function modelLoaded() {
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if(object_status != "")
+    {
+        objectDetector.detect(video,gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("no_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
